0a05b2ed6475f0ffc5afe071c110575ca8590f3c
iannathan-k <ianmtv@gmail.com>
2026-02-26 15:49:43 -0800

    working on output

--- main.cpp
+++ main.cpp
@@ -4,6 +4,47 @@ using namespace std;
 
 // COMSC-210 | Lab 14 | Ian Kusmiantoro
 
+class Color {
+    private:
+        int red;
+        int green;
+        int blue;
+
+    public:
+        void setRed(int red) {this->red = red;};
+        void setGreen(int green) {this->green = green;};
+        void setBlue(int blue) {this->blue = blue;};
+        int getRed() const {return red;}
+        int getGreen() const {return green;}
+        int getBlue() const {return blue;}
+        void print() const {cout << "R: " << red << ", G: " << green << ", B: " << blue << endl;};
+};
+
 int main() {
+    Color color1;
+    Color color2;
+    Color color3;
+
+    color1.setRed(56);
+    color1.setGreen(243);
+    color1.setBlue(229);
+
+    color2.setRed(182);
+    color2.setGreen(130);
+    color2.setBlue(236);
+
+    color3.setRed(67);
+    color3.setGreen(21);
+    color3.setBlue(69);
+
+    cout << "===== Color 1 =====" << endl;
+    color1.print();
+
+    cout << "===== Color 2 =====" << endl;
+    color2.print();
+
+    cout << "===== Color 3 =====" << endl;
+    color3.print();
+
     return 0;
 }
